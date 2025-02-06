ee5ae9fc52307581767874922dc518d976ea9129
Anasmalik57 <itsanas474@gmail.com>
2025-02-06 17:03:54 +0530

    responsiveness added

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ postInputField.addEventListener("input", () => {
     postbtn1.classList.replace("bg-opacity-80", "bg-opacity-100"); // Change to white when input starts
   }
 });
+// ////////////////////////////////
+const foryoubtn = document.querySelector(".foryou");
+const followingbtn = document.querySelector(".followingbtn");
+const trackme = document.querySelector(".trackeme");
+
+// Ensure the elements exist before adding event listeners
+// if (foryoubtn && followingbtn && trackme) {
+//   foryoubtn.addEventListener("click", () => {
+//     // Add fallback in case the classes don't exist
+//     trackme.classList.remove("left-[17%]", "lg:left-[17%]");
+//     trackme.classList.add("left-[63%]", "lg:left-[63%]");
+//   });
+
+//   followingbtn.addEventListener("click", () => {
+//     // Add fallback in case the classes don't exist
+//     trackme.classList.remove("left-[63%]", "lg:left-[63%]");
+//     trackme.classList.add("left-[17%]", "lg:left-[17%]");
+//   });
+// } else {
+//   console.error("One or more elements not found!");
+// }
