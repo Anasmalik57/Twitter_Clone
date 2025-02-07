9bd3e65cce1fa972e39ef9fcf164b1ee32d7f42f
Anasmalik57 <itsanas474@gmail.com>
2025-02-07 13:14:35 +0530

    Terms and conditions added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const postbtn = document.querySelector(".post-btn");
-const postbtn1 = document.querySelector(".postbtnblue");
+// const postbtn1 = document.querySelector(".postbtnblue");
 
 const postInputField = document.querySelector(".post-input-field");
 
 // Initially set the button to red
 postbtn.classList.replace("bg-opacity-100", "bg-opacity-70");
-postbtn1.classList.replace("bg-opacity-100", "bg-opacity-80");
+// postbtn1.classList.replace("bg-opacity-100", "bg-opacity-80");
 
 // Listen for input changes in the field
 postInputField.addEventListener("input", () => {
@@ -13,10 +13,10 @@ postInputField.addEventListener("input", () => {
 
   if (inputValue === "") {
     postbtn.classList.replace("bg-opacity-100", "bg-opacity-70"); // Keep the button red if input is empty
-    postbtn1.classList.replace("bg-opacity-100", "bg-opacity-80"); // Keep the button red if input is empty
+    // postbtn1.classList.replace("bg-opacity-100", "bg-opacity-80"); // Keep the button red if input is empty
   } else {
     postbtn.classList.replace("bg-opacity-70", "bg-opacity-100"); // Change to white when input starts
-    postbtn1.classList.replace("bg-opacity-80", "bg-opacity-100"); // Change to white when input starts
+    // postbtn1.classList.replace("bg-opacity-80", "bg-opacity-100"); // Change to white when input starts
   }
 });
 // ////////////////////////////////
